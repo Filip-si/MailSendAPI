c517397415039f7ba1996ca5fce80d6c6c8de7fb
Filip Siepracki <filip.siepracki@silevis.com>
2021-11-05 12:42:44 +0100

    delete unnecessary

--- Application/Models/Host/HostRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Application.Models.Host
-{
-  public class HostRequest
-  {
-    public string HostName { get; set; }
-
-    public int Port { get; set; }
-  }
-}
--- MailSendAPI/Controllers/MailsController.cs
+++ MailSendAPI/Controllers/MailsController.cs
@@ -2,9 +2,6 @@
 using Application.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace MailSendAPI.Controllers
