c841dd6d2f2ed6d29809ed953eeb9067434fbc71
Filip Siepracki <filip.siepracki@silevis.com>
2021-11-08 10:34:46 +0100

    Add db context migrations and dependencies infrastructure

---
+++ Infrastructure/DatabaseContext.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure
+{
+  public class DatabaseContext : DbContext
+  {
+    public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+    {
+    }
+
+    public DbSet<MailMessageTemplate> MailMessageTemplates { get; set; }
+  }
+}
---
+++ Infrastructure/Migrations/20211107222147_Initial.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MailMessageTemplates",
+                columns: table => new
+                {
+                    MailMessageTemplateId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Subject = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Body = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MailMessageTemplates", x => x.MailMessageTemplateId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MailMessageTemplates");
+        }
+    }
+}
