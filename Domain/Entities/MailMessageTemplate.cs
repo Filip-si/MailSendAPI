d6ca7df5e677c96c3112af46bee6e106d5f4bc0b
Filip Siepracki <filip.siepracki@silevis.com>
2021-11-08 10:36:21 +0100

    Domain model template of mail message

---
+++ Domain/Entities/MailMessageTemplate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Domain.Entities
+{
+  public class MailMessageTemplate
+  {
+    public Guid MailMessageTemplateId { get; set; }
+
+    public string Subject { get; set; }
+
+    public string Body { get; set; }
+  }
+}
