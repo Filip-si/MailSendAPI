522089379c54805ce88811b2d6934e0415fa2e8c
Filip Siepracki <filip.siepracki@silevis.com>
2021-11-23 14:18:53 +0100

    Validation

---
+++ Application/Validators/RecepientRequestValidator.cs
@@ -0,0 +1,15 @@
+﻿using Application.Models;
+using FluentValidation;
+
+namespace Application.Validators
+{
+  public class RecepientRequestValidator : AbstractValidator<RecepientRequest>
+  {
+    public RecepientRequestValidator()
+    {
+      RuleFor(x => x.Email)
+        .MaximumLength(255)
+        .NotEmpty();
+    }
+  }
+}
