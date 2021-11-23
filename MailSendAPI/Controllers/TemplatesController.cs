d01f2ec44372d42c0373e2d11da71ab50a46eca8
Filip Siepracki <filip.siepracki@silevis.com>
2021-11-23 14:08:45 +0100

    Added endpoints GetTemplateHtml and GetTemplateNewsletter, AddTemplateNewsletter and refactor AddTemplateHtml controller

--- MailSendAPI/Controllers/TemplatesController.cs
+++ MailSendAPI/Controllers/TemplatesController.cs
@@ -2,6 +2,7 @@
 using Application.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -10,11 +11,11 @@ namespace MailSendAPI.Controllers
 {
   [ApiController]
   [Route("api/templates")]
-  public class TemplateController : ControllerBase
+  public class TemplatesController : ControllerBase
   {
     private readonly ITemplateService _templateService;
 
-    public TemplateController(ITemplateService templateService)
+    public TemplatesController(ITemplateService templateService)
     {
       _templateService = templateService;
     }
@@ -28,12 +29,39 @@ namespace MailSendAPI.Controllers
       return Ok(await _templateService.GetTemplates());
     }
 
-    [HttpPost]
+    [HttpGet("html-template")]
+    [Produces("application/json")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesDefaultResponseType(typeof(IEnumerable<TemplateResponse>))]
+    public async Task<IActionResult> GetTemplatesHtml()
+    {
+      return Ok(await _templateService.GetTemplatesHtml());
+    }
+
+    [HttpGet("newsletter-template")]
+    [Produces("application/json")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesDefaultResponseType(typeof(IEnumerable<TemplateResponse>))]
+    public async Task<IActionResult> GetTemplatesNewsletter()
+    {
+      return Ok(await _templateService.GetTemplatesNewsletter());
+    }
+
+    [HttpPost("html-template")]
     [ProducesResponseType(StatusCodes.Status201Created)]
     [ProducesDefaultResponseType(typeof(Guid))]
-    public async Task<IActionResult> AddTemplate([FromForm] FileRequest fileRequest, string from, string to)
+    public async Task<IActionResult> AddTemplateHtml([FromQuery] DataRequest dataRequest, [FromForm] FileRequest fileRequest)
     {
-      var templateId = await _templateService.AddTemplate(fileRequest, from, to);
+      var templateId = await _templateService.AddTemplateHtml(fileRequest, dataRequest);
+      return StatusCode(StatusCodes.Status201Created, templateId);
+    }
+
+    [HttpPost("newsletter-template")]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesDefaultResponseType(typeof(Guid))]
+    public async Task<IActionResult> AddTemplateNewsletter([FromForm] FileNewsletterRequest fileNewsletterRequest)
+    {
+      var templateId = await _templateService.AddTemplateNewsletter(fileNewsletterRequest);
       return StatusCode(StatusCodes.Status201Created, templateId);
     }
 
