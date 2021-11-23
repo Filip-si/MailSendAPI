a5543435c3dfe2aee859248b5218ec15d594ae5d
Filip Siepracki <filip.siepracki@silevis.com>
2021-11-23 14:04:14 +0100

    Models of templates

--- Application/Models/Templates/NewsletterModel.cs
+++ Application/Models/Templates/NewsletterModel.cs
@@ -1,14 +1,10 @@
 ﻿namespace Application.Models.Templates
 {
-  public class BasicModel
+  public class NewsletterModel
   {
-    public string DataTemplate { get; set; }
-    public string TextTemplate { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
-    public string FileHeaderData { get; set; }
     public string FileBodyData { get; set; }
     public string FileFooterData { get; set; }
-    public string FileAttachmentData { get; set; }
   }
 }
